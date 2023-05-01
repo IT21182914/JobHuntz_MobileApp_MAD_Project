81cc2cd010b815ab9ec562e56b2d6fce4ccd6fb0
IT21177828 <it21177828@my.sliit.lk>
2023-05-02 01:27:45 +0530

    added comment

--- app/src/main/java/com/example/madproject/activities/ChangePassword.kt
+++ app/src/main/java/com/example/madproject/activities/ChangePassword.kt
@@ -8,5 +8,6 @@ class ChangePassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_passwoed)
+        //Hello
     }
 }
