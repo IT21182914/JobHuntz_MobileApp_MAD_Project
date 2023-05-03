0919a3fbfcfddaf01571b90ba3d91586e8d550ac
rasanginanayakkara <it21178436@my.sliit.lk>
2023-05-03 16:26:25 +0530

    change the text hint

---
+++ app/src/main/java/com/example/madproject/models/PostVacancyModel.kt
@@ -0,0 +1,7 @@
+package com.example.madproject.models
+
+data class PostVacancyModel(
+    var postID : String? = null,
+    var companyName : String? = null,
+    var description : String? = null
+)
