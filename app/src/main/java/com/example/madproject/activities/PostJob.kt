9479592b86e2263988ad2feb8d191f445bb3c227
IT21177828 <it21177828@my.sliit.lk>
2023-05-04 11:43:42 +0530

    change routine in the app

--- app/src/main/java/com/example/madproject/activities/PostJob.kt
+++ app/src/main/java/com/example/madproject/activities/PostJob.kt
@@ -1,20 +1,17 @@
 package com.example.madproject.activities
 
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.madproject.R
-import com.example.madproject.activities.PostVC
 
 class PostJob : AppCompatActivity() {
 
     private lateinit var current: Button
     private lateinit var posts: Button
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_jobs)
